bcfa312e7cd61da2ae7c16b7c3bb14d5a4330e65
Mykola Fedotov <fedotovns@gmail.com>
2023-05-17 14:47:56 +0200

    added history to game

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,7 @@
 export default function Square({ value, onSquareClick }) {
-  // const [value, setValue] = useState(null);
-  // function handleClick() {
-  //   this.props.setValue('X');
-  // }
   return (
-    <div>
-      <button className="square" onClick={onSquareClick}>
-        {value}
-      </button>
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
